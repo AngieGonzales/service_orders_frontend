49a9ca9ed7ad1af7961a2f05b7996be33c66ddcd
AngieGonzales <angkgonzales@soy.sena.edu.co>
2025-04-02 18:01:24 -0500

    feat: Create service order format (advance)

---
+++ pages/form.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center">
+        
+        <div class="row w-75">
+            <div class="col-8">
+                <img src="../public/has.png" alt="" class="w-25">
+            </div>
+            <div class="col-4">
+                <h3>N° ORDEN</h3>
+                <input type="text">
+            </div>
+
+            <div class="col-12 form-floating mt-5 mb-2">
+                <input type="text"  id="company" class="form-control w-100" placeholder="">
+                <label for="company" class="form-label fw-bold ms-2">EMPRESA</label>
+            </div>
+            <div class="col-12 form-floating mb-2">
+                <input type="text" id="sede" class="form-control" placeholder="">
+                <label for="sede" class="form-label fw-bold ms-2">SEDE</label>
+            </div>
+            <div class="col-6 form-floating mb-2">
+                <input type="text" id="address" class="form-control w-100" placeholder="">
+                <label for="address" class="form-label fw-bold ms-2">DIRECCION</label>
+            </div>
+            <div class="col-6 form-floating mb-2">
+                <input type="text" id="city" class="form-control w-100" placeholder="">
+                <label for="city" class="form-label fw-bold ms-2">CIUDAD</label>
+            </div>
+            <div class="col-6 form-floating mb-2">
+                <input type="text" id="contact" class="form-control" placeholder="">
+                <label for="contact" class="form-label fw-bold ms-2">CONTACTO</label>
+            </div>
+            <div class="col-6 form-floating mb-2">
+                <input type="text" id="cc" class="form-control" placeholder="">
+                <label for="cc" class="form-label fw-bold ms-2">C.C</label>
+            </div>
+            <div class="col-3 form-floating mb-2">
+                <input type="text" id="cel" class="form-control" placeholder="">
+                <label for="cel" class="form-label fw-bold ms-2">TELEFONO</label>
+            </div>
+            <div class="col-3 form-floating mb-2">
+                <input type="text" id="cell" class="form-control" placeholder="">
+                <label for="cell" class="form-label fw-bold ms-2">CELULAR</label>
+            </div>
+            <div class="col-6 form-floating">
+                <input type="email" id="email" class="form-control" placeholder="">
+                <label for="email" class="form-label fw-bold ms-2">MAIL</label>
+            </div>
+
+            <div class="m-3">
+                <h2 class="text-center">Información del servicio</h2>
+            </div>
+            <table class="table table-bordered text-start">
+                <thead>
+                    <tr>
+                        <th class="text-center" colspan="2">TIPO DE SERVICIO</th>
+                        <th class="text-center">N° PARTE</th>
+                        <th class="text-center">NOMBRE DEL EQUIPO</th>
+                        <th class="text-center">N° SERIE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="2" class="w-50">
+                            <textarea class=" text form-control" rows="3"></textarea>
+                        </td>
+                        <td>
+                            <textarea class=" text form-control" rows="3"></textarea>
+                        </td>
+                        <td>
+                            <textarea class=" text form-control" rows="3"></textarea>
+                        </td>
+                        <td>
+                            <textarea class=" text form-control" rows="3"></textarea>
+                        </td>
+                    </tr>
+                </tbody>
+        </table>
+        <table class="table table-bordered text-start m-4">
+                <thead>
+                    <tr>
+                        <th class="text-center" colspan="2">DESCRIPCION DEL ESTADO EN EL QUE SE RECIBE</th>
+                        <th class="text-center">Fecha y Responsable</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="2" class="w-75">
+                            <textarea class=" text form-control" rows="6"></textarea>
+                        </td>
+                        <td>
+                            <input type="date" class="form-control mb-2">
+                            <input type="text" class="form-control" placeholder="Responsable">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered text-start m-4">
+                <thead>
+                    <tr>
+                        <th class="text-center" colspan="2">SOLUCIÓN</th>
+                        <th class="text-center">Fecha y Responsable</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="2" class="w-75">
+                            <textarea class=" text form-control" rows="6" placeholder="Escribe la solución aquí..."></textarea>
+                        </td>
+                        <td>
+                            <input type="date" class="form-control mb-2">
+                            <input type="text" class="form-control" placeholder="Responsable">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="m-3">
+                <h2 class="text-center">Entrega</h2>
+            </div>
+            <table class="table table-bordered text-start m-4">
+                <thead>
+                    <tr>
+                        <th class="text-center" colspan="2">RECOMENDACIONES</th>
+                        <th class="text-center">Firma y Fecha</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="2" class="w-75">
+                            <textarea class=" text form-control" rows="6"></textarea>
+                        </td>
+                        <td>
+                            
+                            <input type="date" class="form-control mb-2">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<style>
+.form-control {
+    border-color: #878888;
+}
+.text {
+    border: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
     <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
     </head>
+
     <div class="container d-flex justify-content-center">
         <div class="row mt-5 w-75 shadow-lg">
             <div class="col bg rounded-4">
                 <div class="d-flex justify-content-center mt-5">
-                    <img src="../public/user.png" alt="" class="image">
+                    <img src="../public/user.png" alt="" class="image" />
                 </div>
                 <div>
                     <h2 class="message text-white text-center">Welcome to the Service Orders System</h2>
@@ -16,84 +17,72 @@
             <div class="col h-75">
                 <h2 class="title fw-bold text-center mb-5">BIENVENIDO</h2>
                 <form action="#" class="form">
+                <div class="md-4">
+                    <label for="email" class="form-label fw-bold">Correo Electrónico</label>
+                    <input type="email" class="form-control p-3 mb-4 shadow" name="email" id="email" placeholder="Enter your  email"/>
+                </div>
 
-                    <div class="md-4">
-                        <label for="email" class="form-label fw-bold">Correo Electrónico</label>
-                        <input type="email" class="form-control p-3 mb-4 shadow" name="email" id="email" placeholder="Enter your  email">
+                <div class="md-4 position-relative">
+                    <label for="password" class="form-label fw-bold">Contraseña</label>
+                    <div class="input-group shadow rounded">
+                        <input id="password" type="password" class="form-control border-end-0 p-3" name="password" placeholder="Enter your password" required/>
+                        <span class="input-group-text bg-transparent border border-start-0 pe-3" @click="toggleInputPassword">
+                            <i class="fas fa-eye"></i>
+                        </span>
                     </div>
+                </div>
 
-                    <!-- <div class="md-4 position-relative">
-                        <label for="password" class="form-label fw-bold">Contraseña</label>
-                        <div class="d-flex align-items-center">
-
-                            <input id="password" type="password" class="form-control p-3 shadow" name="password" placeholder="Enter your password" required>
-                            <i class="fas fa-eye pe-3" @click="toggleInputPassword"></i>
-                             <i class="fas fa-eye position-absolute end-0 translate-middle-y pe-3" @click="toggleInputPassword"></i> 
-                        </div>
-                    </div> -->
-
-                    <div class="md-4 position-relative">
-    <label for="password" class="form-label fw-bold">Contraseña</label>
-    <div class="input-group shadow rounded">
-        <input id="password" type="password" class="form-control border-end-0 p-3" name="password" placeholder="Enter your password" required>
-        <span class="input-group-text bg-transparent border border-start-0 pe-3" @click="toggleInputPassword">
-            <i class="fas fa-eye"></i>
-        </span>
-    </div>
-</div>
-
-
-                    <div class="d-grid mt-5">
-                        <button type="submit" class="btn p-3 text-white fw-bold">Acceder</button>
-                    </div>
-                    
+                <div class="d-grid mt-5">
+                    <button type="submit" class="btn p-3 text-white fw-bold">Acceder</button>
+                </div>
                 </form>
             </div>
         </div>
-    </div>  
+    </div>
 </template>
 
 <script setup>
-const toggleInputPassword=()=> {
-        const input = document.getElementById('password');
-        const button = document.getElementById('password-button');
+const toggleInputPassword = () => {
+    const input = document.getElementById("password");
+    const button = document.getElementById("password-button");
         if (input.type === "password") {
             input.type = "text";
-            button.classList.remove('btn-outline-secondary');
-            button.classList.add('btn-outline-primary');
+            button.classList.remove("btn-outline-secondary");
+            button.classList.add("btn-outline-primary");
         } else {
             input.type = "password";
-            button.classList.remove('btn-outline-primary');
-            button.classList.add('btn-outline-secondary');
+            button.classList.remove("btn-outline-primary");
+            button.classList.add("btn-outline-secondary");
         }
-    }
+};
 </script>
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Boldonse&display=swap');
 
-    .bg {
-        background-image: url(../public/bg.jpeg);
-        background-position: center center;
-        height: 500px;
-    }
-    .title {
-        margin-top: 30%;
-        font-family: 'Boldonse';
-    }
-    .message {
-        font-family: 'Boldonse';
-    }
-    .form-label {
-        color: #868a89;
-    }
-    .btn {
-        background-color: #bd53f9;
-    }
-    .form {
-        width: 270px;
-        margin-left: 70px;
-    }
-    .image {
-        width: 100px;
-    }
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Boldonse&display=swap");
+
+.bg {
+    background-image: url(../public/bg.jpeg);
+    background-position: center center;
+    height: 500px;
+}
+.title {
+    margin-top: 30%;
+    font-family: "Boldonse";
+}
+.message {
+    font-family: "Boldonse";
+}
+.form-label {
+    color: #868a89;
+}
+.btn {
+    background-color: #bd53f9;
+}
+.form {
+    width: 270px;
+    margin-left: 70px;
+}
+.image {
+    width: 100px;
+}
 </style>
