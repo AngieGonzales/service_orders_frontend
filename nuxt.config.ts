e4b5b699e281a29abfc435e769910d7787049254
AngieGonzales <angkgonzales@soy.sena.edu.co>
2025-04-09 15:21:42 -0500

    chore: install useBootstrap and define enviroment variable.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  css: ['bootstrap/dist/css/bootstrap.min.css'],
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   modules: [
+    'usebootstrap',
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
