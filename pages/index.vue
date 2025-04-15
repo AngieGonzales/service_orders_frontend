5f7093485d506cf42ed428d5d76d784268e55a5a
AngieGonzales <angkgonzales@soy.sena.edu.co>
2025-04-15 17:53:51 -0500

    refactor: modify responsiveness login view

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,38 @@
 <template>
-    <div class="container d-flex justify-content-center">
-        <div class="row mt-5 w-75 shadow-lg">
-            <div class="col bg rounded-4">
-                <div class="d-flex justify-content-center mt-5">
-                    <img src="../public/user.png" alt="" class="image" />
-                </div>
-                <div>
-                    <h2 class="message text-white text-center">Bienvenido</h2>
-                </div>
-                <div class="msg">
-                    <h2 class="message text-white text-center">Sistema de ordenes de servicio</h2>
-                </div>
-            </div>
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <div class="row shadow-lg w-100" style="max-width: 860px;">
+        <div class="col-md-6 bg rounded-start-4 d-flex flex-column justify-content-center align-items-center text-center text-white p-4">
+            <img src="../public/user.png" alt="" class="image mb-3" />
+            <h2 class="message p-5 ">Bienvenido</h2>
+            <h2 class="message">Sistema de ordenes de servicio</h2>
+        </div>
 
-            <div class="col h-75">
-                <h2 class="title fw-bold text-center mb-5 fs-2">BIENVENIDO</h2>
-                <form action="#" class="form">
-                <div class="md-4">
+        <div class="col-md-6 bg-white rounded-end-4 p-4 d-flex flex-column justify-content-center">
+            <h2 class="title fw-bold text-center mb-4 fs-2">BIENVENIDO</h2>
+            <form action="#" class="w-100 px-3">
+                <div class="mb-4">
                     <label for="email" class="form-label fw-bold">Correo Electrónico</label>
-                    <input type="email" class="form-control p-3 mb-4 shadow" name="email" id="email" placeholder="Ingrese su email"/>
+                    <input type="email" class="form-control p-3 shadow" name="email" id="email" placeholder="Ingrese su email" />
                 </div>
 
-                <div class="md-4 position-relative">
+                <div class="mb-4 position-relative">
                     <label for="password" class="form-label fw-bold">Contraseña</label>
                     <div class="input-group shadow rounded">
-                        <input id="password" type="password" class="form-control border-end-0 p-3" name="password" placeholder="Ingrese su contraseña" required/>
+                        <input id="password" type="password" class="form-control border-end-0 p-3" name="password" placeholder="Ingrese su contraseña" required />
                         <span class="input-group-text bg-transparent border border-start-0 pe-3" @click="toggleInputPassword">
                             <i class="fas fa-eye"></i>
                         </span>
                     </div>
                 </div>
 
-                <div class="d-grid mt-5">
-                    <button type="submit" class="btn p-3 text-white fw-bold" >Acceder</button>
+                <div class="d-grid mt-4">
+                    <button type="submit" class="btn p-3 text-white fw-bold">Acceder</button>
                 </div>
-                </form>
-            </div>
+            </form>
         </div>
     </div>
+</div>
 </template>
-
 <script setup>
 
 definePageMeta({
@@ -60,35 +53,32 @@ const toggleInputPassword = () => {
         }
 };
 </script>
-
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Boldonse&display=swap");
 
 .bg {
     background-image: url(../public/bg.jpeg);
-    background-position: center center;
-    height: 500px;
+    background-size: cover;
+    background-position: center;
+    min-height: 500px;
 }
+
 .title {
-    margin-top: 30%;
     font-family: "Boldonse";
 }
+
 .message {
     font-family: "Boldonse";
 }
-.msg {
-    margin-top: 35%;
-}
+
 .form-label {
     color: #868a89;
 }
+
 .btn {
     background-color: #bd53f9;
 }
-.form {
-    width: 270px;
-    margin-left: 70px;
-}
+
 .image {
     width: 100px;
 }
