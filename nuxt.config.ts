0f517bdca1571a300095c56758f13d14a4438699
AngieGonzales <angkgonzales@soy.sena.edu.co>
2025-04-21 16:34:01 -0500

    fix: bootstrap error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  components: true,
   css: ['bootstrap/dist/css/bootstrap.min.css'],
   vite: {
     define: {
